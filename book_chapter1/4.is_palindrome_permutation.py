718007a63cdd603195951edaaf3914352dbd9fbd
Igor Kaseko <igorksk@gmail.com>
2025-07-12 10:15:39 +0300

    is palindrome permutation

---
+++ book_chapter1/4.is_palindrome_permutation.py
@@ -0,0 +1,26 @@
+str = "Tact Coa"
+
+def is_palindrome_permutation(str):
+
+    from collections import defaultdict
+
+    counts = defaultdict(int)
+
+    for symbol in str:
+        if symbol == ' ':
+            continue  # Ignore spaces
+
+        lower_symbol = symbol.lower()
+
+        counts[lower_symbol] += 1
+
+    odd_count = 0
+
+    for count in counts.values():
+        if count % 2 == 1:
+            odd_count += 1
+
+    # Can be only 0 or 1 odd chars count
+    return odd_count < 2
+
+print(is_palindrome_permutation(str))
