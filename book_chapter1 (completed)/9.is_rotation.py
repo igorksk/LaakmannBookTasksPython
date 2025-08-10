76833aa50643c6b02cec5a9ed4332ee58ec8ebb6
Igor Kaseko <igorksk@gmail.com>
2025-08-10 14:24:49 +0300

    complete 1st chapter

---
+++ book_chapter1 (completed)/9.is_rotation.py
@@ -0,0 +1,14 @@
+
+
+def is_rotation(s1, s2):
+    """
+    Check if s2 is a rotation of s1 using only one call to is_substring.
+    """
+    if len(s1) != len(s2) or not s1:
+        return False
+    return s2 in (s1 + s1)
+
+str1 = "waterbottle";
+str2 = "erbottlewat";
+
+print("Is '{}' a rotation of '{}'? {}".format(str2, str1, is_rotation(str1, str2)))
